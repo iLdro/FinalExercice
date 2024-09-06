e7b60185a980e0b81b2600b33ac6ff8821b69560
Julien Klein <44435573+iLdro@users.noreply.github.com>
2024-09-06 18:30:21 +0200

    Final commit exo css

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const stepsData = [
     hints: ["Utiliser le raccourci ! + entrer", "Utiliser la balise <title>"]
   },
   {
-    title: "Hearder",
+    title: "Header",
     details: ["Créer un header et lui ajouter un id header"],
     hints: ["Créer une balise header dans le body et lui mettre id=\"header\""]
   },
@@ -18,9 +18,9 @@ const stepsData = [
     hints: ["Utiliser la balise h1 et un attribut : class"]
   },
   {
-    title: "Navgation Bar",
+    title: "Navigation Bar",
     details: ["Ajouter dans le header un menu de navigation. Utiliser une liste non ordonée. Cette liste devra contenir des liens vers les sections A Propos, Services et Contact"],
-    hints: ["Utiliser les balises <nav>, <ul>, <li>, <a> et l'attribut href"]
+    hints: ["Utiliser les balises <nav>, <ul>, <li>, <a> et l'attribut href vers les ID #about, #services et #contact."]
   },
   {
     title: "Section A Propos partie 1",
@@ -58,12 +58,12 @@ const stepsData = [
     hints: ["Pour réaliser le formulaire il faudra utiliser la balise <form>, une syntaxe de ce type pour le nom et l'email : <label for='name'>Nom :</label> et la balise <button> avec un type submit"]
   },
   {
-    title: "Étape 12",
+    title: "Ajouter un footer",
     details: ["Ajouter un footer avec le texte suivant : &copy; 2024 Mon site avec Train d'Avenir"],
     hints: ["Utilisez les balises <footer> et <p>"]
   },
   {
-    title : ["Ajouter un id au footer"],
+    title : ["Ajouter un ID au footer"],
     details : ["Ajouter l'id footer au footer"],
     hints : ["<footer id=\"footer\""]
   },
@@ -75,41 +75,113 @@ const stepsData = [
 
 
   {
-    title: "Étape CSS",
+    title: "Créer et utiliser son fichier",
     details: ["Créer le fichier style.css puis le relier au fichier HTML."],
-    hints: ['Ajouter la ligne <link r"l="stylesheet] href="style.css"']
+    hints: ['Ajouter la ligne <link rel="stylesheet" href="style.css"']
   },
 
   {
-    title: "Étape CSS",
-    details: ["Appliquer la couleur #4CAF50 au fond au hearder"],
-    hints: ['a.	Pour modifier le header, écrire « header » en tant que sélecteur, puis ouvrir des accolades { }. Dans les accolades, écrire la propriété « background-color » pour modifier la couleur du fond. Appliquer la couleur à « #4CAF50 ».']
+    title: "Modifier le header partie 1",
+    details: ["Appliquer la couleur #4CAF50 au fond au header en utilisant son ID"],
+    hints: ['Pour modifier le header, écrire « #header » en tant que sélecteur, puis ouvrir des accolades { }. Dans les accolades, écrire la propriété « background-color » pour modifier la couleur du fond. Appliquer la couleur à « #4CAF50 ».']
   },
   {
-    title: "Etape CSS",
+    title: "Modifier le header partie 2",
     details:["Mettre le texte du header en blanc"],
     hints:["Pour modifier la couleur du texte, écrire la propriété « color » dans les accolades du sélecteur « header ». Appliquer la couleur à « white ». "]
   },
   {
-    title: "Etape CSS",
-    details:["Changer notre class title définit précédemment pour que le text ait une taille de 24px, et une margin bottom de 10px"],
+    title: "Modifier le titre",
+    details:["Changer notre class title définit précédemment pour que le texte ait une taille de 24px, et une marge de 10px en dessous."],
     hints : ["Ouvrir des accolades après le sélecteur « .title ». Écrire la propriété « font-size » pour modifier la taille du texte. Appliquer la taille à « 24px ». Écrire la propriété « margin-bottom » pour modifier la marge en bas du texte. Appliquer la marge à « 10px ». "]
   },
   {
-    title: "Etape CSS",
-    details:["Crer un grouping selector pour le header, footer et section"],
+    title: "Grouping Selector partie 1",
+    details:["Créer un grouping selector pour le header, footer et section"],
     hints : ["Pour créer un groupe de sélecteurs, écrire les sélecteurs « header, footer, section » séparés par des virgules. Ouvrir des accolades après les sélecteurs. Écrire les propriétés « font-family » et « color » pour modifier la police et la couleur du texte. Appliquer la police à « Arial » et la couleur à « black ». "]
   },
   {
-    title: "Etape CSS",
+    title: "Grouping Selector partie 2",
     details:["Centrer le texte dans le groupe de sélecteurs"],
     hints : ["Pour centrer le texte, écrire la propriété « text-align » dans les accolades du groupe de sélecteurs. Appliquer l’alignement à « center ». "]
   },
   {
-    title: "Etape CSS",
+    title: "Grouping Selector partie 3",
     details:["Ajouter au sélecteur des marges et du padding de 20px"],
-    hints : [""]
-  }
+    hints : ["Ajouter les lignes margin:20px; et padding:20px;"]
+  },
+
+  {
+    title: "Modifier le style global avec body",
+    details:["Modifier la police des textes dans body en Arial"],
+    hints : ["Ecrire body en tant que sélecteur et écrire font-family:Arial, sans-sherif;"]
+  },
+
+  {
+    title: "Modifier le style global avec body",
+    details:["Supprimer les marges et les paddings."],
+    hints : ["Fixer margin:0px; et padding:0px;"]
+  },
+
+  {
+    title: "Modifier la liste de navigation - ul",
+    details:["Définir ul en tant que sélecteur, puis supprimer les puces de la liste avec list-style-type:none;. Ensuite, enlever le padding."],
+    hints : ["ul {\nlist-style-type:none;\npadding:0;\n}"]
+  },
+
+  {
+    title: "Modifier la liste de navigation - ul li",
+    details:["Afficher les éléments en ligne. Ajouter une marge latérale de 10px pour les espacer."],
+    hints : ["Pour aligner des éléments, il faut utiliser display:inline. Pour ajouter une marge latérale, il faut ajouter 10px uniquement à droite et à gauche. ul li {\ndisplay:inline;\nmargin:0 10px;\n}"]
+  },
+
+  {
+    title: "Modifier la liste de navigation - ul a",
+    details:['Mettre le texte des liens (balise <a>) en blanc. Supprimer les soulignements avec "text-decoration:none"'],
+    hints : ["ul a {color:white; text-decoration:none;}"]
+  },
+
+  {
+    title: "Styliser le formulaire",
+    details:["On va utiliser un Grouping Selector pour cibler les champs <input> et <button>."],
+    hints : ['Le sélecteur doit ressembler à : "input, button". On ouvre ensuite les accolades.']
+  },
+
+  {
+    title: "Styliser le formulaire",
+    details:["Ajouter une marge verticale et un padding de 10."],
+    hints : ['Après les sélecteurs, ajouter entre les accolades "margin:10px 0; padding:10px;']
+  },
+
+  {
+    title: "Styliser le formulaire",
+    details:['Ajouter une bordure avec la ligne "border:1px solid #ccc". Ajuster ensuite la largeur à 80% en ajoutant la ligne "width:80%;"'],
+    hints : ['Le style du formulaire doit ressembler à cela : "input, button { margin:10px 0; padding:10px; border: 1px solid #ccc; width: 80%;}"']
+  },
+
+  {
+    title: "Styliser le bouton",
+    details:['Avec le selecteur "button", mettre le background en blanc et supprimer la bordure. Enfin ajouter la ligne "cursor:pointer" pour changer le curseur au survol du bouton.'],
+    hints : ['button { background:white; border: none; cursor:pointer;}']
+  },
+
+  {
+    title: "Styliser le bouton :hoover",
+    details:['Nous allons faire en sorte que la couleur du bouton change quand on le survole avec la souris. Créer un nouveau sélecteur "button:hoover" avec la propriété "background-color:#45a049".'],
+    hints : ['button:hoover { background:#45a049; }']
+  },
+
+  {
+    title: "Modifier le footer",
+    details:['Modifier le le footer en utilisant son ID. Mettre la couleur du background à #333, mettre le texte en blanc et ajouter un padding de 10px.'],
+    hints : ['#footer { background:#333; color:white; padding:10px; }']
+  },
+
+  {
+    title: "C'est fini !",
+    details:['Vous avez fini. Si vous vous ennuyez, vous pouvez visiter les sites suivants : https://flukeout.github.io/ ou https://flexboxfroggy.com'],
+    hints : ['🥳']
+  },
 
   // Ajoute plus d'étapes si nécessaire
 ];
